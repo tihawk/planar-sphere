2355c9fef0f2c5b8ed9e8c577c130e2266feab57
Glitch (quixotic-sphere) <none>
2017-11-25 06:51:04 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -7,20 +7,21 @@
 $(function() {
   console.log('hello world :o');
   
-  $.get('/dreams', function(dreams) {
+  /*$.get('/dreams', function(dreams) {
     dreams.forEach(function(dream) {
       $('<li></li>').text(dream).appendTo('ul#dreams');
     });
-  });
+  });*/
 
   $('form').submit(function(event) {
     event.preventDefault();
-    var dream = $('input').val();
+    window.location.href="/api/whoami";
+    /*var dream = $('input').val();
     $.post('/dreams?' + $.param({dream: dream}), function() {
       $('<li></li>').text(dream).appendTo('ul#dreams');
       $('input').val('');
       $('input').focus();
-    });
+    });*/
   });
 
 });
